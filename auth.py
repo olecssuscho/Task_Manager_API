3e7235f431bcb75f550bc5544f2b2abf9d6c4f8e
olecs <olecs@SANYA>
2026-07-14 17:27:39 +0300

    create auth module

---
+++ auth.py
@@ -0,0 +1,33 @@
+from jose import JWTError, jwt # type: ignore
+from config import settings
+from passlib.context import CryptContext  # type: ignore
+from datetime import datetime,timedelta,timezone
+
+contex = CryptContext(schemes=["bcrypt","argon2"], deprecated = "auto")
+
+def hash_password(password:str)->str:
+    return contex.hash(password)
+
+def check_password(password:str,password_to_check:str)->bool:
+    return contex.verify(password,password_to_check)
+
+def create_refresh_token(data:dict)->str:
+    to_encode = data.copy()
+    to_encode.update({"type":"refresh"})
+    exp = datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_TIME_DAYS)
+    to_encode.update({"exp":exp})
+    return jwt.encode(to_encode,settings.SECRET_KEY,settings.ALGHORITHM)
+
+def create_access_token(data:dict)->str:
+    to_encode = data.copy()
+    to_encode.update({"type":"access"})
+    exp = datetime.now(timezone.utc) + timedelta(days=settings.REFRESH_TOKEN_TIME_DAYS)
+    to_encode.update({"exp":exp})
+    return jwt.encode(to_encode,settings.SECRET_KEY,settings.ALGHORITHM)
+
+def decode_token(token:str)->dict:
+    try:
+        payload = jwt.decode(token,settings.SECRET_KEY,settings.ALGHORITHM)
+        return payload
+    except JWTError:
+        return None
--- config.py
+++ config.py
@@ -4,7 +4,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
 
     DB_URL:str
-
+    REFRESH_TOKEN_TIME_DAYS:int
+    ACCESS_TOKEN_TIME_MINUTES:int
+    ALGHORITHM:str
+    SECRET_KEY:str
     model_config = ConfigDict(env_file = ".env", env_file_encoding = "utf-8")
 
 settings = Settings()
