dc9c0fc896edf26d53c239f7d94cc05c97a4a41c
olecs <olecs@SANYA>
2026-07-15 13:25:21 +0300

    made correct db connection

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
-from sqlalchemy.ext.asyncio import create_async_engine,async_session 
+from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker
 from config import settings
 
-async_engine = create_async_engine(url = settings.DB_URL, echo = True , autocommit=False , autoflush = False)
+async_engine = create_async_engine(url = settings.DB_URL, echo = True)
 
-async_Session = async_session()
+Async_Session_Local = async_sessionmaker(async_engine,autocommit=False, autoflush=False)
