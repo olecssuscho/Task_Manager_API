4af5fa6102d38951fc9e7b6eebab9236d0e9222b
olecs <olecs@SANYA>
2026-07-19 12:59:56 +0300

    add methodt that allow to add member due id

--- services/project_members.py
+++ services/project_members.py
@@ -17,7 +17,25 @@ async def add_member_services(project_id:int,user:UserDB,db:AsyncSession):
         await db.commit()
         await db.refresh(project_member_db)
         return project_member_db
-    
+
+async def add_member_id_services(project_id:int,user_id:int,role:ProjectMemberDB,user:UserDB,db:AsyncSession):
+    stmt = await db.execute(select(ProjectDB).filter(ProjectDB.id == project_id))
+    if stmt.scalar_one_or_none() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Project did not found")
+    user_db = await db.execute(select(UserDB).filter(UserDB.id == user_id))
+    if not user_db.scalar_one_or_none():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User did not found")
+    else:
+        project_member_db = ProjectMemberDB(
+            project_id = project_id,
+            user_id = user_id,
+            role = role.role
+        )
+        db.add(project_member_db)
+        await db.commit()
+        await db.refresh(project_member_db)
+        return project_member_db  
+
 async def delete_member_services(project_id:int,user_id:int,user:UserDB,db:AsyncSession):
     stmt = await db.execute(select(ProjectDB).filter(ProjectDB.id == project_id))
     result = stmt.scalar_one_or_none()
