766e5f356f43d6b32906712d12a4f49035f8c7a6
Olecs <olecssuscochina@gmail.com>
2026-09-03 18:58:44 +0300

    change argument in func

--- routers/files.py
+++ routers/files.py
@@ -8,12 +8,12 @@ from fastapi.responses import FileResponse
 router = APIRouter(prefix="/file", tags=["files"])
 
 @router.post("/upload")
-async def upload_file(file: UploadFile, task_id:int, project_id:int, user:UserMODELS = Depends(get_current_user),db:AsyncSession = Depends(get_db)):
-    return await upload_file_services(file,file.filename,task_id,project_id,user,db)
+async def upload_file(file: UploadFile, task_id:int, user:UserMODELS = Depends(get_current_user),db:AsyncSession = Depends(get_db)):
+    return await upload_file_services(file,file.filename,task_id,user,db)
 
 @router.get("/download")
-async def download_file(filename:str, task_id:int, project_id:int, user:UserMODELS = Depends(get_current_user),db:AsyncSession = Depends(get_db)):
-    file = await download_file_services(filename,task_id,project_id,user,db)
+async def download_file(filename:str, task_id:int, user:UserMODELS = Depends(get_current_user),db:AsyncSession = Depends(get_db)):
+    file = await download_file_services(filename,task_id,user,db)
     return FileResponse(
         path=file.file_path,
         filename=file.filename,
--- services/files.py
+++ services/files.py
@@ -9,14 +9,14 @@ from depends import get_role
 import mimetypes
 
 
-async def upload_file_services(file:UploadFile, original_filename:str, id:int, project_id:int, user:UserDB, db:AsyncSession):
+async def upload_file_services(file:UploadFile, original_filename:str, task_id:TaskDB, user:UserDB, db:AsyncSession):
 
-    await get_role(project_id,"editor",user,db)
+    await get_role(task_id.project_id,"editor",user,db)
 
     if file.size > settings.MAX_FILE_SIZE:
         raise HTTPException(status_code=status.HTTP_413_CONTENT_TOO_LARGE, detail="File size too big")
 
-    stmt = await db.execute(select(TaskDB).filter(TaskDB.id == id, TaskDB.project_id == project_id))
+    stmt = await db.execute(select(TaskDB).filter(TaskDB.id == task_id.id, TaskDB.project_id == task_id.project_id))
 
     if not stmt.scalar_one_or_none():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task or project not found")
@@ -33,14 +33,14 @@ async def upload_file_services(file:UploadFile, original_filename:str, id:int, p
 
     content_type,_ = mimetypes.guess_type(path)
     if not content_type:
-        return "application/octet-stream"
+        content_type = "application/octet-stream"
 
     file_db = AttachmentDB(
         filename = file.filename,
         file_path = path,
         content_type = content_type,
         size_bytes = file.size,
-        task_id = id,
+        task_id = task_id.project_id,
         uploaded_by = user.id
     )
 
@@ -48,20 +48,20 @@ async def upload_file_services(file:UploadFile, original_filename:str, id:int, p
     await db.commit()
     return "Success"
 
-async def download_file_services(original_filename:str, id:int, project_id:int, user:UserDB, db:AsyncSession):
-    stmt = await db.execute(select(TaskDB).filter(TaskDB.id == id, TaskDB.project_id == project_id))
+async def download_file_services(original_filename:str, task_id:TaskDB, user:UserDB, db:AsyncSession):
+    stmt = await db.execute(select(TaskDB).filter(TaskDB.id == task_id.id, TaskDB.project_id == task_id.project_id))
 
     if not stmt.scalar_one_or_none():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task or project not found") 
 
-    file = await db.execute(select(AttachmentDB).filter(AttachmentDB.filename == original_filename))
+    file = await db.execute(select(AttachmentDB).filter(AttachmentDB.filename == original_filename,AttachmentDB.task_id == id))
 
     file_real = file.scalar_one_or_none()
 
     if not file_real:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="File is not found")
 
-    await get_role(project_id,"viewer",user,db)
+    await get_role(task_id.project_id,"viewer",user,db)
 
     return file_real
 
