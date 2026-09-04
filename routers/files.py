f4958f2a8cc222d55ccf7fe3b158601e887ddfe9
Olecs <olecssuscochina@gmail.com>
2026-09-04 18:44:56 +0300

    make content-disposition

--- routers/files.py
+++ routers/files.py
@@ -1,6 +1,7 @@
 from schemas.models import UserMODELS
 from depends import get_db,get_current_user
-from fastapi import APIRouter,Depends,UploadFile,BackgroundTasks
+from fastapi import APIRouter,Depends,UploadFile
+from starlette.background import BackgroundTask
 from sqlalchemy.ext.asyncio import AsyncSession
 from services.files import upload_file_services,download_file_services
 from fastapi.responses import StreamingResponse
@@ -18,4 +19,4 @@ async def download_file(filename:str, task_id:int, user:UserMODELS = Depends(get
     file = await download_file_services(filename,task_id,user,db)
     customer = minio.Minio(settings.MINIO_ENDPOINT, settings.MINIO_ACCESS_KEY, settings.MINIO_SECRET_KEY, secure=False)
     response = customer.get_object(settings.MINIO_BUCKET, file.file_path)
-    return StreamingResponse(response, media_type=file.content_type, headers={"Content-Disposition": f'attachment; filename="{file.filename}"'},background=BackgroundTasks(response.close))
+    return StreamingResponse(response, media_type=file.content_type, headers={"Content-Disposition": f'attachment; filename="{file.filename}"'},background=BackgroundTask(response.close))
