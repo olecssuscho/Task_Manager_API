f3dd4dbabe675966ea363ad97dbfd4b946c5c0bd
Olecs <olecssuscochina@gmail.com>
2026-09-04 18:11:47 +0300

    made change to file system to work correcnr at container

--- config.py
+++ config.py
@@ -11,8 +11,13 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_TIME_MINUTES:int
     ALGORITHM:str
     SECRET_KEY:str
-    UPLOAD_DIR: str 
     MAX_FILE_SIZE:int
+    MINIO_ROOT_USER:str
+    MINIO_ROOT_PASSWORD:str
+    MINIO_ENDPOINT:str
+    MINIO_ACCESS_KEY:str
+    MINIO_SECRET_KEY:str  
+    MINIO_BUCKET:str
     model_config = ConfigDict(env_file = ".env.local", env_file_encoding = "utf-8")
 
 settings = Settings()
