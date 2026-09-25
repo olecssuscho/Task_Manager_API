386b7fd29c7f3a18723dfa3e7cc074b9936a7785
Olecs <olecssuscochina@gmail.com>
2026-09-25 17:42:34 +0300

    change variables

--- services/ai_client.py
+++ services/ai_client.py
@@ -3,15 +3,14 @@ from anthropic import Anthropic
 from config import settings
 from schemas.models import TaskMODELS
 from voyageai.client import Client
-from typing import Literal
 from schemas.responces import SuggestRESPONSES
 
 voyage_client = Client(api_key=settings.VOYAGE_API_KEY)
 
-cluaud_client = Anthropic(api_key=settings.CLAUDE_API_KEY)
+claude_client = Anthropic(api_key=settings.CLAUDE_API_KEY)
 
 def generate_task_data_from_text(text:str):
-    response = cluaud_client.messages.parse(
+    response = claude_client.messages.parse(
         model="claude-opus-5",
         max_tokens=200,
         messages=[
@@ -37,25 +36,22 @@ def generate_task_data_from_text(text:str):
     return response.parsed_output
 
 def generate_embedding(text:str):
-    responce = voyage_client.embed(texts=text,model="voyage-3.5",input_type="document")
+    responce = voyage_client.embed(texts=[text],model="voyage-3.5",input_type="document")
     return responce.embeddings[0]
 
 def suggest(text:list[str]):
-    responce = cluaud_client.messages.parse(
+    responce = claude_client.messages.parse(
         model="claude-opus-5",
         max_tokens=200,
         messages=[
             {
                 "role":"user",
                 "content":f"""
-                read this text {text} and suggest priority level:{Literal["low","medium","high"]} as priority,
+                read this text {text} and suggest priority level ("low","medium","high") as priority,
                 and give reason why this priority as reasoning
                 Make:
                 -priority
                 -reasoning
-                important rule:
-                - Interpret relative dates such as "today", "tomorrow", "next week",
-                    and "next month" relative to the Current date{datetime.now(timezone.utc)}.
                 """
             }
         ],
